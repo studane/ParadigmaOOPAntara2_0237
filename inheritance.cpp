0845880ba2bda93e69c90013f5cb79cf9bb3e9c1
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-13 13:41:37 +0700

    membuat function perkenalan

--- inheritance.cpp
+++ inheritance.cpp
@@ -40,4 +40,10 @@ public:
         cout << "Pelajar dihapus\n" << endl;
     }
 
-    
+    //pembuatan function perkenalan()
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah
+            + "\n\n";
+    }
+
+};
