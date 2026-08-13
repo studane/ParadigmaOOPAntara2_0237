68219cbdc93ccf96ce1fe8ecd5fee1e197713e90
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-13 13:36:08 +0700

    membuat variabel private dan public

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class remoteLampu 
+class remoteLampu {
+private:
+    string saklarNo[5];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
